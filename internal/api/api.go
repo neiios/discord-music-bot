f95cb367782be58b616cd6614b35b87306da3e5a
Igor <igor@sgf.lt>
2026-02-27 21:53:59 +0200

    Remove unused API methods and types

--- internal/api/api.go
+++ internal/api/api.go
@@ -59,30 +59,6 @@ func NewClient(url string, token string) (*Client, error) {
 	return client, nil
 }
 
-func (c *Client) RegisterDefaultCommands() error {
-	req := CreateGlobalApplicationCommandRequest{
-		Name:        "play",
-		Type:        1,
-		Description: "Play a song from URL",
-		Options: []ApplicationCommandOption{
-			{
-				Type:        3,
-				Name:        "url",
-				Description: "The URL of the song",
-				Required:    true,
-			},
-			{
-				Type:        3,
-				Name:        "timestamp",
-				Description: "The timestamp to start playing from",
-				Required:    false,
-			},
-		},
-	}
-
-	return c.CreateGlobalApplicationCommand(req)
-}
-
 func (c *Client) GetCurrentApplication() (Application, error) {
 	res, err := c.Client.Get(fmt.Sprintf("%s/applications/@me", c.BaseUrl))
 	if err != nil {
@@ -113,54 +89,6 @@ func (c *Client) GetGatewayUrl() (string, error) {
 	return response.Url, nil
 }
 
-func (c *Client) CreateGlobalApplicationCommand(req CreateGlobalApplicationCommandRequest) error {
-	body, err := json.Marshal(req)
-	if err != nil {
-		return err
-	}
-
-	res, err := c.Client.Post(
-		fmt.Sprintf("%s/applications/%s/commands", c.BaseUrl, c.AppId),
-		"application/json",
-		bytes.NewBuffer(body),
-	)
-	if err != nil {
-		return err
-	}
-	defer res.Body.Close()
-
-	if b, err := io.ReadAll(res.Body); err == nil {
-		println("Response body:", string(b))
-	}
-
-	if res.StatusCode != http.StatusOK {
-		return fmt.Errorf("failed to create command, status code: %d", res.StatusCode)
-	}
-
-	return nil
-}
-
-func (c *Client) DeleteGlobalApplicationCommand(commandID string) error {
-	req, err := http.NewRequest(http.MethodDelete, fmt.Sprintf("%s/applications/%s/commands/%s", c.BaseUrl, c.AppId, commandID), nil)
-	if err != nil {
-		fmt.Println("Error creating request:", err)
-		return err
-	}
-
-	resp, err := c.Client.Do(req)
-	if err != nil {
-		fmt.Println("Error sending request:", err)
-		return err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusNoContent {
-		return fmt.Errorf("failed to delete command, status code: %d", resp.StatusCode)
-	}
-
-	return nil
-}
-
 func (c *Client) SendMessage(channelID, content string) error {
 	body, err := json.Marshal(CreateMessageRequest{Content: content})
 	if err != nil {
@@ -178,28 +106,8 @@ func (c *Client) SendMessage(channelID, content string) error {
 	defer res.Body.Close()
 
 	if res.StatusCode != http.StatusOK {
-		return fmt.Errorf("failed to send message, status code: %d", res.StatusCode)
-	}
-
-	return nil
-}
-
-func (c *Client) BulkOverwriteGlobalApplicationCommands() error {
-	req, err := http.NewRequest(http.MethodPut, fmt.Sprintf("%s/applications/%s/commands", c.BaseUrl, c.AppId), nil)
-	if err != nil {
-		fmt.Println("Error creating request:", err)
-		return err
-	}
-
-	resp, err := c.Client.Do(req)
-	if err != nil {
-		fmt.Println("Error sending request:", err)
-		return err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("failed to delete command, status code: %d", resp.StatusCode)
+		b, _ := io.ReadAll(res.Body)
+		return fmt.Errorf("failed to send message, status code: %d, body: %s", res.StatusCode, b)
 	}
 
 	return nil
--- internal/api/types.go
+++ internal/api/types.go
@@ -1,19 +1,5 @@
 package api
 
-type ApplicationCommandOption struct {
-	Type        int    `json:"type"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Required    bool   `json:"required"`
-}
-
-type CreateGlobalApplicationCommandRequest struct {
-	Name        string                     `json:"name"`
-	Type        int                        `json:"type"`
-	Description string                     `json:"description"`
-	Options     []ApplicationCommandOption `json:"options,omitempty"`
-}
-
 type CreateMessageRequest struct {
 	Content string `json:"content"`
 }
